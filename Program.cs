658464951eadea73770304243243e520620a2161
torvic <torvic02st96@gmail.com>
2023-07-06 22:01:03 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Humanizer;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Por favor ingrese un nombre");
+var nombre = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese cargo");
+var cargo = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su edad");
+var edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad.ToWords()}.");
